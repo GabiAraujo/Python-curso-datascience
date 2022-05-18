1616f9b9fb34add6e32d6cdcfd981541c9bb101e
Gabriela Araujo <gabiaraujo2739@gmail.com>
2022-05-18 16:35:54 -0300

    Delete adc_item_lista.py

--- adc_item_lista.py
+++
@@ -1,9 +0,0 @@
-l1 = ('aileron', 'flap', 'leme', 'profundor')
-i = 0
-surperficies_comando = list(l1)
-while i < 2:
-    sc = input('digite mais uma superficie de comando:')
-    surperficies_comando.append(sc)
-    i+=1
-print(surperficies_comando)
-
